ca600527404810b0be526caf038be533b0d0a094
Halina Tulchii <halendeihalyna@gmail.com>
2023-07-18 19:47:58 +0300

    summary hw03

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const ctrl = require("../../controllers/contacts")
 
-const { validateBody } = require('../../middlewares');
+const { validateBody, isValidId } = require('../../middlewares');
 
-const schemas = require('../../schemas/contacts')
+const {schemas} = require('../../models/contact')
 
 const router = express.Router();
 
@@ -11,13 +11,16 @@ const router = express.Router();
 
 router.get('/', ctrl.getAll);
 
-router.get('/:contactId', ctrl.getById);
+router.get('/:contactId',isValidId, ctrl.getById);
 
 
 router.post('/', validateBody(schemas.addSchema), ctrl.add);
 
-router.delete('/:contactId', ctrl.remove);
+router.delete('/:contactId', isValidId, ctrl.remove);
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrl.update);
+router.put('/:contactId', isValidId, validateBody(schemas.addSchema), ctrl.update);
+
+router.patch('/:contactId/favorite', isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+
 
 module.exports = router
